63c30fb774e5c4bae27b698063536d8c246150cc
Dennis de Oliveira Py <dennispy14@hotmail.com>
2023-05-30 20:27:36 -0300

    primeiro comit

---
+++ appagtarefas/appagtarefas/src/main/java/com/appagtarefas/appagtarefas/AppagtarefasApplication.java
@@ -0,0 +1,13 @@
+package com.appagtarefas.appagtarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppagtarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppagtarefasApplication.class, args);
+	}
+
+}
---
+++ appagtarefas/appagtarefas/src/test/java/com/appagtarefas/appagtarefas/AppagtarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appagtarefas.appagtarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppagtarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
